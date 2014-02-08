49809bf5d9b9de4a80163d8dcea53595fd19b9e0
Brian Rak <dn@devicenull.org>
2014-02-08 12:24:39 -0500

    Refactor all footer operations out into their own class

---
+++ ipmifw/FirmwareImage.py
@@ -0,0 +1,80 @@
+
+import struct
+
+class FirmwareImage:
+	# indicates the image will be used during boot
+	IMAGE_ACTIVE = 0x01
+	# indicates it should be copied from flash to ram on boot.  Copies *from* the load address to the base address, but only if it's active
+	IMAGE_COPY2RAM = 0x02
+	# indicates control should be passed to the image (how? I dunno)
+	IMAGE_EXEC = 0x04
+	# indicates this is a filesystem image
+	IMAGE_FILE = 0x08
+	# indicates the image is compressed.  Supposedly via ZIP
+	IMAGE_COMPRESSED = 0x10
+
+	# This data comes from the "W90P710 Bootloader Users Manual" (find it yourself, I'm not allowed to distribute it)
+	footer_format = "<5I16s4I"
+
+        # I largely don't have any idea how or why this works.  It was basically just coding by trival and error.
+        # I'm unsure of why they used this, versus something more standard
+        @staticmethod
+        def computeChecksum(data):
+                cksum = 0xffffffff
+
+                for i in range(0,len(data)):
+                        char = ord(data[i])
+
+                        cksum -= char << ((i%4)*8)
+
+                        if cksum < 0:
+                                cksum &= 0xffffffff
+                                cksum -= 1
+
+                return cksum
+
+	def __init__(self):
+		self.imagenum = 0
+		self.base_addr = 0
+		self.length = 0
+		self.load_address = 0
+		self.exec_address = 0
+		self.name = 0
+		self.image_checksum = 0
+		self.signature = 0
+		self.type = 0
+		self.footer_checksum = 0
+		
+	def loadFromString(self,footer): 
+		(self.imagenum, self.base_addr, self.length, self.load_address, self.exec_address, self.name, self.image_checksum, self.signature, self.type, self.footer_checksum) = struct.unpack(FirmwareImage.footer_format,footer)
+
+	# The footer checksum is only the 48 bytes of actual data (excludes the \xff padding, and the checksum itself)
+	def computeFooterChecksum(self):
+		footer_data = struct.pack("<5I16s3I",self.imagenum, self.base_addr, self.length, self.load_address, self.exec_address, self.name, self.image_checksum, self.signature, self.type)
+		return FirmwareImage.computeChecksum(footer_data)
+
+
+	def __str__(self):
+        	flag_desc = []
+        	if self.type & FirmwareImage.IMAGE_ACTIVE:
+                	flag_desc.append('active')
+        	if self.type & FirmwareImage.IMAGE_COPY2RAM:
+                	flag_desc.append('copy2ram')
+        	if self.type & FirmwareImage.IMAGE_EXEC:
+                	flag_desc.append('exec')
+        	if self.type & FirmwareImage.IMAGE_FILE:
+                	flag_desc.append('file')
+	        if self.type & FirmwareImage.IMAGE_COMPRESSED:
+       	        	flag_desc.append('compressed')
+
+		description = "Firmware image: %i Name: %s Base: 0x%x Length: 0x%x (%i) Load: 0x%x Exec: 0x%x Image Checksum: 0x%x Signature: 0x%x Type: %s (0x%x) Footer Checksum: 0x%x" % (self.imagenum, self.name, self.base_addr, self.length, self.length, self.load_address, self.exec_address, self.image_checksum, self.signature, ', '.join(flag_desc), self.type, self.footer_checksum)
+
+		computed_footer_checksum = self.computeFooterChecksum()
+
+		if self.footer_checksum == computed_footer_checksum:
+			description += " * footer checksum matches"
+		else:
+			description += " * footer checksum mismatch, expected 0%x" % computed_footer_checksum
+
+		return description		
+
--- read_header.py
+++ read_header.py
@@ -1,45 +1,15 @@
 
-# some example headers, not used for anything but reference
-headers = [
-	"\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\x02\x00\x00\x00\x00\x00\x18\x40\x00\xe0\x7b\x00\x00\x00\xd0\x00\x00\x00\xd0\x00\x31\x73\x74\x46\x53\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x0a\x2c\xc1\xae\x9f\xff\xff\xa0\x08\x00\x00\x00\xc7\x80\x96\x27\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff",
-	"\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\x03\x00\x00\x00\x00\x00\x98\x40\xa8\x2a\x11\x00\x00\x80\x00\x00\x00\x80\x00\x00\x6b\x65\x72\x6e\x65\x6c\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x23\xf5\xcc\x9c\x9f\xff\xff\xa0\x17\x00\x00\x00\xaa\x0e\x16\x13\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff",
-	"\x29\x9f\x17\xff\xff\xff\xff\xff\xff\xff\xff\xff\x04\x00\x00\x00\x00\x00\xb8\x40\x00\x10\x1e\x00\x00\x00\xd0\x00\x00\x00\xd0\x00\x32\x6e\x64\x46\x53\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\xac\x1c\x9c\x9a\x9f\xff\xff\xa0\x08\x00\x00\x00\x22\x65\x89\x3b\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff"
-]
+import re, hashlib, os
+from ipmifw.FirmwareImage import FirmwareImage
 
-# I largely don't have any idea how or why this works.  It was basically just coding by trival and error.
-# I'm unsure of why they used this, versus something more standard
-def computeChecksum(data):
-	cksum = 0xffffffff
-
-	for i in range(0,len(data)):
-       		char = ord(data[i])
-
-        	pos = char << ((i%4)*8)
-
-        	cksum -= pos
-
-        	if cksum < 0:
-                	cksum &= 0xffffffff
-                	cksum -= 1
-
-	return cksum
-
-
-# indicates the image must be processed? at boot time
-IMAGE_ACTIVE = 0x01
-# indicates it should be copied from flash to ram on boot.  Copies *from* the load address to the base address, but only if it's active
-IMAGE_COPY2RAM = 0x02
-# indicates control should be passed to the image (how? I dunno)
-IMAGE_EXEC = 0x04
-# indicates this is a filesystem image
-IMAGE_FILE = 0x08
-# indicates the image is compressed.  Supposedly via ZIP
-IMAGE_COMPRESSED = 0x10
-
-import struct, re, hashlib
 
 with open('SMT_313.bin','r') as f:
 	ipmifw = f.read()
+
+try:
+	os.mkdir('data')
+except OSError:
+	pass
 
 print "Read %i bytes" % len(ipmifw)
 bootloader = ipmifw[:64040]
@@ -48,60 +18,38 @@ bootloader_md5 = hashlib.md5(bootloader).hexdigest()
 if bootloader_md5 != "166162c6c9f21d7a710dfd62a3452684":
 	print "Warning: bootloader (first 64040 bytes of file) md5 doesn't match.  This parser may not work with a different bootloader"
 	print "Expected 166162c6c9f21d7a710dfd62a3452684, got %s" % bootloader_md5
-	print "Dumping bootloader to bootloader.bin and continuing..."
-	with open('bootloader.bin','w') as f:
-		f.write(bootloader)
 else:
 	print "Bootloader md5 matches, this parser will probably work!"
 
-# This data comes from the "W90P710 Bootloader Users Manual" (find it yourself, I'm not allowed to distribute it)
-#
+print "Dumping bootloader to data/bootloader.bin"
+with open('data/bootloader.bin','w') as f:
+	f.write(bootloader)
+
 # I'm not terribly happy with this.  We're reading through a file, looking for the pattern that identifies a block of data.
 # Is the controller really doing this on bootup?  The way we're doing it is a horrible abuse of regular expressions!
 # We rely on the \xff(9) ... \x9f\xff\xff\xa0 ... \xff(16) signature to check.  Hopefully that doesn't occur otherwise :)
 for (part1, part2) in re.findall("\xff{9}(.{40})\x9f\xff\xff\xa0(.{8})\xff{16}",ipmifw,re.DOTALL):
-	header = "%s\x9f\xff\xff\xa0%s" % (part1, part2)
+	footer = "%s\x9f\xff\xff\xa0%s" % (part1, part2)
 
-	(imagenum, base_addr, length, load_address, exec_address, name, image_checksum, signature, type, footer_checksum) = struct.unpack("<5I16s4I",header)
-
-	flag_desc = []
-	if type & IMAGE_ACTIVE:
-		flag_desc.append('active')
-	if type & IMAGE_COPY2RAM:
-		flag_desc.append('copy2ram')
-	if type & IMAGE_EXEC:
-		flag_desc.append('exec')
-	if type & IMAGE_FILE:
-		flag_desc.append('file')
-	if type & IMAGE_COMPRESSED:
-		flag_desc.append('compressed')
-
-	print "\nImage: %i Name: %s Base: 0x%x Length: 0x%x (%i) Load: 0x%x Exec: 0x%x Image Checksum: 0x%x Signature: 0x%x Type: %s (0x%x) Footer Checksum: 0x%x" % (imagenum, name, base_addr, length, length, load_address, exec_address, image_checksum, signature, ', '.join(flag_desc), type, footer_checksum)
+	fi = FirmwareImage()
+	fi.loadFromString(footer)
 
 
-	# The footer checksum is only the 48 bytes of actual data (excludes the \xff padding, and the checksum itself)
-	computed_footer_checksum = computeChecksum(header[:-4])
-	if computed_footer_checksum != footer_checksum:
-		print "Warning: Footer checksum mismatch, footer: 0x%x, computed 0x%x" % (footer_checksum,computed_footer_checksum)
-	else:
-		print "Footer checksum matches"
+	print "\n"+str(fi)
 
-	
-
-
-	imagestart = base_addr
-	if base_addr > 0x40000000:
+	imagestart = fi.base_addr
+	if fi.base_addr > 0x40000000:
 		# I'm unsure where this 0x40000000 byte offset is coming from.  Perhaps I'm not parsing the footer correctly?
 		imagestart -= 0x40000000
 
-	imageend = imagestart + length
+	imageend = imagestart + fi.length
 
-	print "Dumping 0x%s to 0x%s to %s.bin" % (imagestart, imageend, name)
-	with open('%s.bin' % name.replace("\x00",""),'w') as f:
+	print "Dumping 0x%s to 0x%s to data/%s.bin" % (imagestart, imageend, fi.name)
+	with open('data/%s.bin' % fi.name.replace("\x00",""),'w') as f:
 		f.write(ipmifw[imagestart:imageend])
-		computed_image_checksum = computeChecksum(ipmifw[imagestart:imageend])
+		computed_image_checksum = FirmwareImage.computeChecksum(ipmifw[imagestart:imageend])
 
-		if computed_image_checksum != image_checksum:
-			print "Warning: Image checksum mismatch, footer: 0x%x computed: 0x%x" % (image_checksum,computed_image_checksum)
+		if computed_image_checksum != fi.image_checksum:
+			print "Warning: Image checksum mismatch, footer: 0x%x computed: 0x%x" % (fi.image_checksum,computed_image_checksum)
 		else:
 			print "Image checksum matches"
