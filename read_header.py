750c73230e3c185ea192ed4eff5fab1fa4842724
Brian Rak <dn@devicenull.org>
2014-02-09 10:47:38 -0500

    Add command line arguments for reading filename and controlling extraction

--- read_header.py
+++ read_header.py
@@ -1,9 +1,13 @@
 #!/usr/local/bin/python2.7
 
-import re, hashlib, os, io
+import re, hashlib, os, io, argparse, sys
 from ConfigParser import ConfigParser
 from ipmifw.FirmwareImage import FirmwareImage
 
+cmdparser = argparse.ArgumentParser(description='Read and extract data from SuperMicro IPMI firmware')
+cmdparser.add_argument('--extract',action='store_true',help='Extract any detected firmware images')
+cmdparser.add_argument('filename',help='Filename to read from')
+args = cmdparser.parse_args()
 
 default_ini = """
 [flash]
@@ -15,7 +19,7 @@ total_size=0
 config = ConfigParser()
 config.readfp(io.BytesIO(default_ini))
 
-with open('SMT_313.bin','r') as f:
+with open(args.filename,'r') as f:
 	ipmifw = f.read()
 
 config.set('flash', 'total_size', len(ipmifw))
@@ -35,9 +39,10 @@ if bootloader_md5 != "166162c6c9f21d7a710dfd62a3452684":
 else:
 	print "Bootloader md5 matches, this parser will probably work!"
 
-print "Dumping bootloader to data/bootloader.bin"
-with open('data/bootloader.bin','w') as f:
-	f.write(bootloader)
+if args.extract:
+	print "Dumping bootloader to data/bootloader.bin"
+	with open('data/bootloader.bin','w') as f:
+		f.write(bootloader)
 
 # I'm not terribly happy with this.  We're reading through a file, looking for the pattern that identifies a block of data.
 # Is the controller really doing this on bootup?  The way we're doing it is a horrible abuse of regular expressions!
@@ -58,15 +63,16 @@ for (part1, part2) in re.findall("\xff{9}(.{40})\x9f\xff\xff\xa0(.{8})\xff{16}",
 
 	imageend = imagestart + fi.length
 
-	print "Dumping 0x%s to 0x%s to data/%s.bin" % (imagestart, imageend, fi.name)
-	with open('data/%s.bin' % fi.name.replace("\x00",""),'w') as f:
-		f.write(ipmifw[imagestart:imageend])
-		computed_image_checksum = FirmwareImage.computeChecksum(ipmifw[imagestart:imageend])
+	if args.extract:
+		print "Dumping 0x%s to 0x%s to data/%s.bin" % (imagestart, imageend, fi.name)
+		with open('data/%s.bin' % fi.name.replace("\x00",""),'w') as f:
+			f.write(ipmifw[imagestart:imageend])
+			computed_image_checksum = FirmwareImage.computeChecksum(ipmifw[imagestart:imageend])
 
-		if computed_image_checksum != fi.image_checksum:
-			print "Warning: Image checksum mismatch, footer: 0x%x computed: 0x%x" % (fi.image_checksum,computed_image_checksum)
-		else:
-			print "Image checksum matches"
+			if computed_image_checksum != fi.image_checksum:
+				print "Warning: Image checksum mismatch, footer: 0x%x computed: 0x%x" % (fi.image_checksum,computed_image_checksum)
+			else:
+				print "Image checksum matches"
 
 
 	config.set('images', str(fi.imagenum), 'present')
@@ -79,5 +85,9 @@ for (part1, part2) in re.findall("\xff{9}(.{40})\x9f\xff\xff\xa0(.{8})\xff{16}",
 	config.set(configkey, 'name', fi.name)
 	config.set(configkey, 'type', hex(fi.type))
 
-with open('data/image.ini','w') as f:
-	config.write(f)
+if args.extract:
+	with open('data/image.ini','w') as f:
+		config.write(f)
+else:
+	print "\nConfiguration info:\n"
+	config.write(sys.stdout)
